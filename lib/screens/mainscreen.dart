7ea2bafd213decd0ca281aa6d1f601f1831a0d6a
brocode <kelimbth@gmail.com>
2023-02-28 16:35:12 +0300

    created a stateful widget

---
+++ lib/screens/mainscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayer extends StatefulWidget {
+  const MusicPlayer({Key? key}) : super(key: key);
+
+  @override
+  State<MusicPlayer> createState() => _MusicPlayerState();
+}
+
+class _MusicPlayerState extends State<MusicPlayer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
